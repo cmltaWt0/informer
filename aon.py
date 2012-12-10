fb6705226919665c1bb45714958704bb2f8d754c
Maksim Sokolsky <misokolsky@gmail.com>
2012-12-10 11:17:26 +0200

    Make refactoring and refinemant to aon.py. Add writing to log file.

--- aon.py
+++ aon.py
@@ -1,24 +1,15 @@
-#!/usr/bin/python
-#encoding: utf-8
-
 import telnetlib
 #import smtplib
 import os
 import time
-from viewer import view_tk
+from viewer.view_tk import view_tk
 
 
 """
 AON service.
 """
 
-year = str(time.localtime().tm_year)
-mon = str(time.localtime().tm_mon)
-day = str(time.localtime().tm_mday)
-hour = str(time.localtime().tm_hour)
-minuts = str(time.localtime().tm_min)
-
-host = "192.168.52.6"  # Sip-server IP-address
+host = "192.168.52.6"  # Log-server IP-address
 
 
 file_out = os.path.dirname(os.path.realpath(__file__)) + '/log/' + \
@@ -31,18 +22,17 @@ file_err = os.path.dirname(os.path.realpath(__file__)) + '/log/' + \
             '.' + str(time.localtime().tm_year) + '-aon.err'
 
 str_search = '577140400'  # Searched phone number
-#FROM = "maksim@laptop.localdomain"
-#TO = ["maksim@laptop.localdomain"]
-#SUBJECT = "Incoming Call." # Not utilizing yet
+#FROM = "user@localdomain"
+#TO = ["user@localdomain"]
+#SUBJECT = "Incoming Call."
 TEXT = "Incoming call from "
 
 
 def str_parsing(str_all, str_search, pars_id=1):
 
-    """Проверка наличия искомого номера.
-    Функция str_parsing проверяет наличие искомого номера
-    в списке элементов строки с телнет сессии и выдает
-    как результат номер звонивщегоей и время звонка"""
+    """Searching for a number(str_search) in str_all.
+    Assume that str_all and str_search is a not empty string
+    Result = string contaning caller_id and date-time of call"""
 
     if not isinstance(str_all, str) or not isinstance(str_search, str):
         raise TypeError('Input string type value please.')
@@ -61,55 +51,53 @@ def str_parsing(str_all, str_search, pars_id=1):
 #def send_smtp():
 #    try:
 #        server.sendmail(FROM, TO, result)
-#    except:
+#    except Exception as e:
 #        try:
+#            writing_log(file_err, 'Trying to reassing connection. ', e)
 #            server = smtplib.SMTP('localhost')
 #            server.sendmail(FROM, TO, result)
-#           view_tk(result)
-#        except:
-#            with open(file_out, 'a') as f:
-#                f.write('Losing connection to SMTP...' + '\n')
-#                f.write(result + '\n')
-#            view_tk(result)
-#    else:
-#        print(result)
-#    with open('os.getcwd() + '/test', 'a') as f:
-#        f.write(result + '\n')
-#    view_tk(result)
+#        except Exception as e:
+#            writing_log(file_err, 'Losing connection to SMTP... ', e)
+#            writing_log(file_out, result)
 
-if __name__ == "__main__":
+
+def writing_log(file_path, text, err=''):
+    try:
+        with open(file_path, 'a') as f:
+            f.write(str(time.localtime().tm_year) + '-' +
+                    str(time.localtime().tm_mon) + '-' +
+                    str(time.localtime().tm_mday) + '-' +
+                    str(time.localtime().tm_hour) + ':' +
+                    str(time.localtime().tm_min) + ': ' +
+                    text + str(err) + '\n')
+    except IOError as e:
+        view_tk(str(e))
+
+
+def main():
     while True:
         try:
             tn = telnetlib.Telnet(host)
-        except:
+        except Exception as e:
+            writing_log(file_err, 'Connection to server is not available: ', e)
             time.sleep(5)
         else:
             while True:
                 try:
-                    ts = tn.sock_avail()
-                except:
+                    tn.sock_avail()
+                except Exception as e:
+                    writing_log(file_err, 'Connection is not active.', e)
                     time.sleep(5)
                     tn = telnetlib.Telnet(host)
                 else:
-                    str_a = tn.read_until('\r\r\n\r\n')
+                    str_all = tn.read_until('\r\r\n\r\n')
                     try:
-                        call_info = str_parsing(str_a, str_search)  # Results
-                    except:
-                        try:
-                            with open(file_err, 'a') as f:
-                                f.write('Exception: Value or Type (need to \
-                                implement later)' + '\n')
-                        except:
-                            print("Trouble with file_err." + file_err)
+                        call_info = str_parsing(str_all, str_search)  # Parsing
+                    except (TypeError, ValueError) as e:
+                        writing_log(file_err, 'Error parsing: ', e)
                     else:
                         if call_info is not None:  # Not negative result
                             result = (TEXT + str("".join(call_info)))
-
 #                           send_smtp()
-                            try:
-                                with open(file_out, 'a') as f:
-                                    f.write(result + '\n')
-                            except:
-                                print("Trouble with file_out." + file_out)
-
-                            view_tk.view_tk(result)
+                            writing_log(file_out, result)
+                            view_tk(result)
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#encoding: utf-8
+
+"""AON application's goal is to parse some Log-server output and show missed
+or ended call info.
+In the future, the application must be finalized in some GUI-interface.
+Need to implement running in Windows."""
+
+from aon import main
+
+
+if __name__ == "__main__":
+    main()
